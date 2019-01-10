ec1128c4b97d9fd72b9359638d8a315d10c5bbe6
CloudMagician <luzixv1998@icloud.com>
2019-01-10 11:00:27 +0800

    add table and change analysis

--- LP.m
+++ LP.m
@@ -1,17 +1,14 @@
 clc,clear
-k=0;
+a=0.025;
 hold on
-while a<0.05
-    c=[0,0.024,0.016,0.045,0.026];
-    A=diag([-0.05,-0.255,-0.21,-0.195,-0.18]);
-    b=a*ones(4,1);
-    Aeq=[1,1.015,1.02,1.055,1.06];
-    beq=1;
-    LB=zeros(5,1);
-    %[x,Q]=linprog(-c,A,b,Aeq,beq,LB);
-    x = fminimax(fun,x0,A,b,Aeq,beq,lb,ub);
-    Q=-Q;
-    plot(a,Q,'*k');
-    a = a + 0.001;
-end
-xlabel('a'),ylabel('Q')
+
+c=[0.05,0.255,0.21,0.195,0.18];
+A=[zeros(4,1),diag([0.024,0.016,0.045,0.026])];
+b=a*ones(4,1);
+Aeq=[1,1.015,1.02,1.055,1.06];
+beq=1;
+LB=zeros(5,1);
+[x,Q]=linprog(-c,A,b,Aeq,beq,LB);
+Q=-Q;
+disp(Q);
+disp(x);
